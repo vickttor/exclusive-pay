b2651f7425c43f5fca00603d4691446dc3e8311b
Victor H. Silva <victor.silvaPC@hotmail.com>
2024-04-16 10:35:58 -0300

    feat: :zap: add metadatas

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,11 +2,24 @@ import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
 
-const poppins = Poppins({ subsets: ["latin"], weight: ["400", "500", "700", "900"] });
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "500", "700", "900"],
+});
 
 export const metadata: Metadata = {
   title: "Exclusive Pay",
-  description: "Simples pagamentos, conexões reais. Encontre tudo que precisa para processar seus pagamentos digitais com segurança na Exclusive Pay.",
+  description:
+    "Simples pagamentos, conexões reais. Encontre tudo que precisa para processar seus pagamentos digitais com segurança na Exclusive Pay.",
+  robots: "index, follow",
+  keywords:
+    "Pagamentos, Gateway, Payments, Plataforma, Conexões, Processar pagamentos digitais, segurança",
+  openGraph: {
+    images: ["/preview.png"],
+    title: "Exclusive Pay",
+    description:
+      "Simples pagamentos, conexões reais. Encontre tudo que precisa para processar seus pagamentos digitais com segurança na Exclusive Pay.",
+  },
 };
 
 export default function RootLayout({
